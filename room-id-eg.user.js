76b7032214c4f316954607a05b1fe4c624261e50
Clara Jorda <clara.jorda@gmail.com>
2020-02-20 14:20:29 +0100

    version

--- room-id-eg.user.js
+++ room-id-eg.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Expedia Test
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.expedia.com/*.Hotel-Information*
